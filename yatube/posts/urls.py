272df5501b40ca68d684d8c031bc50806d25e661
BishukRoma <r.bishuk@gmail.com>
2023-02-08 11:58:24 +0300

    Сhange program.py

--- yatube/posts/urls.py
+++ yatube/posts/urls.py
@@ -3,9 +3,11 @@ from django.urls import path
 
 from . import views
 
+app_name = 'posts'
+
 urlpatterns = [
-    path('', views.index),
-    path('index.html', views.index),
-    path('group/<slug:slug>/', views.group_posts),
-    path('group_list.html', views.group_list),
+    path('', views.index, name = 'posts'),
+    path('index.html', views.index, name = 'post_index'),
+    path('group/<slug:slug>/', views.group_posts, name = 'group'),
+    path('group_list.html', views.group_list, name = 'group_list'),
 ] 
